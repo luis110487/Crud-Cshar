608e3f240ba30a67a882859f4eef10b56e7e312b
LUIS CARLOS GAMARRA CASTRO <lgamarra3@unisimon.edu.co>
2023-12-28 16:54:35 -0500

    s

---
+++ CRUD_Sol/CRUD_Sol/Logic/Conection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace CRUD_Sol.Logic
+{
+    public class Conection
+    {
+        private string DataBase;
+        private static Conection Con = null;
+
+
+        private Conection()
+        {
+            DataBase = "";
+        }
+
+    }
+}
--- CRUD_Sol/CRUD_Sol/Program.cs
+++ CRUD_Sol/CRUD_Sol/Program.cs
@@ -16,7 +16,7 @@ namespace CRUD_Sol
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Articles());
         }
     }
 }
