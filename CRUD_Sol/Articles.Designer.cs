dc1d1910d4a1a3259bcc4d35fb865dbb4896c363
LUIS CARLOS GAMARRA CASTRO <lgamarra3@unisimon.edu.co>
2023-12-29 14:20:26 -0500

    Create Form

--- CRUD_Sol/Articles.Designer.cs
+++ CRUD_Sol/Articles.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Articles));
             this.dtv_List = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.txtSearch = new System.Windows.Forms.TextBox();
@@ -49,6 +51,7 @@
             this.btnCategory = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dtv_List)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,11 +60,12 @@
             this.dtv_List.AllowUserToAddRows = false;
             this.dtv_List.AllowUserToDeleteRows = false;
             this.dtv_List.AllowUserToOrderColumns = true;
+            this.dtv_List.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
             this.dtv_List.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtv_List.Location = new System.Drawing.Point(30, 95);
+            this.dtv_List.Location = new System.Drawing.Point(28, 97);
             this.dtv_List.Name = "dtv_List";
             this.dtv_List.ReadOnly = true;
-            this.dtv_List.Size = new System.Drawing.Size(686, 420);
+            this.dtv_List.Size = new System.Drawing.Size(686, 435);
             this.dtv_List.TabIndex = 0;
             // 
             // label1
@@ -76,6 +80,7 @@
             // 
             // txtSearch
             // 
+            this.txtSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(194)))), ((int)(((byte)(255)))));
             this.txtSearch.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtSearch.Location = new System.Drawing.Point(121, 61);
             this.txtSearch.Name = "txtSearch";
@@ -84,57 +89,79 @@
             // 
             // btnSearch
             // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(179)))), ((int)(((byte)(179)))));
             this.btnSearch.Location = new System.Drawing.Point(442, 48);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(121, 43);
             this.btnSearch.TabIndex = 3;
             this.btnSearch.Text = "Search";
-            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.UseVisualStyleBackColor = false;
             // 
             // btnNew
             // 
+            this.btnNew.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(179)))), ((int)(((byte)(179)))));
+            this.btnNew.ImageKey = "agregar-archivo.png";
+            this.btnNew.ImageList = this.imageList1;
             this.btnNew.Location = new System.Drawing.Point(398, 559);
             this.btnNew.Name = "btnNew";
             this.btnNew.Size = new System.Drawing.Size(142, 97);
             this.btnNew.TabIndex = 4;
             this.btnNew.Text = "New";
-            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnNew.UseVisualStyleBackColor = false;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
             // 
             // btnUpdate
             // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(179)))), ((int)(((byte)(179)))));
+            this.btnUpdate.ImageKey = "actualizar-datos.png";
+            this.btnUpdate.ImageList = this.imageList1;
             this.btnUpdate.Location = new System.Drawing.Point(546, 559);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(142, 97);
             this.btnUpdate.TabIndex = 4;
             this.btnUpdate.Text = "Update";
-            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnUpdate.UseVisualStyleBackColor = false;
             // 
             // btnDelete
             // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(179)))), ((int)(((byte)(179)))));
+            this.btnDelete.ImageKey = "eliminar.png";
+            this.btnDelete.ImageList = this.imageList1;
             this.btnDelete.Location = new System.Drawing.Point(694, 559);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(142, 97);
             this.btnDelete.TabIndex = 4;
             this.btnDelete.Text = "Delete";
-            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnDelete.UseVisualStyleBackColor = false;
             // 
             // btnReport
             // 
+            this.btnReport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(179)))), ((int)(((byte)(179)))));
+            this.btnReport.ImageKey = "informacion.png";
+            this.btnReport.ImageList = this.imageList1;
             this.btnReport.Location = new System.Drawing.Point(842, 559);
             this.btnReport.Name = "btnReport";
             this.btnReport.Size = new System.Drawing.Size(142, 97);
             this.btnReport.TabIndex = 4;
             this.btnReport.Text = "Report";
-            this.btnReport.UseVisualStyleBackColor = true;
+            this.btnReport.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnReport.UseVisualStyleBackColor = false;
             // 
             // btnClose
             // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(179)))), ((int)(((byte)(179)))));
+            this.btnClose.ImageKey = "cerrar-sesion.png";
+            this.btnClose.ImageList = this.imageList1;
             this.btnClose.Location = new System.Drawing.Point(1000, 559);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(142, 97);
             this.btnClose.TabIndex = 4;
             this.btnClose.Text = "Closes";
-            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnClose.UseVisualStyleBackColor = false;
             // 
             // label2
             // 
@@ -148,9 +175,11 @@
             // 
             // txtArticles
             // 
+            this.txtArticles.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(194)))), ((int)(((byte)(255)))));
             this.txtArticles.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtArticles.Location = new System.Drawing.Point(759, 116);
             this.txtArticles.Name = "txtArticles";
+            this.txtArticles.ReadOnly = true;
             this.txtArticles.Size = new System.Drawing.Size(236, 28);
             this.txtArticles.TabIndex = 2;
             // 
@@ -166,9 +195,11 @@
             // 
             // txtBrand
             // 
+            this.txtBrand.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(194)))), ((int)(((byte)(255)))));
             this.txtBrand.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtBrand.Location = new System.Drawing.Point(759, 197);
             this.txtBrand.Name = "txtBrand";
+            this.txtBrand.ReadOnly = true;
             this.txtBrand.Size = new System.Drawing.Size(236, 28);
             this.txtBrand.TabIndex = 2;
             // 
@@ -184,9 +215,11 @@
             // 
             // txtExtent
             // 
+            this.txtExtent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(194)))), ((int)(((byte)(255)))));
             this.txtExtent.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtExtent.Location = new System.Drawing.Point(759, 271);
             this.txtExtent.Name = "txtExtent";
+            this.txtExtent.ReadOnly = true;
             this.txtExtent.Size = new System.Drawing.Size(236, 28);
             this.txtExtent.TabIndex = 2;
             // 
@@ -202,52 +235,76 @@
             // 
             // txtCategory
             // 
+            this.txtCategory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(194)))), ((int)(((byte)(255)))));
             this.txtCategory.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtCategory.Location = new System.Drawing.Point(762, 349);
             this.txtCategory.Name = "txtCategory";
+            this.txtCategory.ReadOnly = true;
             this.txtCategory.Size = new System.Drawing.Size(236, 28);
             this.txtCategory.TabIndex = 2;
             // 
             // btbExtent
             // 
+            this.btbExtent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(179)))), ((int)(((byte)(179)))));
             this.btbExtent.Location = new System.Drawing.Point(1018, 266);
             this.btbExtent.Name = "btbExtent";
             this.btbExtent.Size = new System.Drawing.Size(57, 33);
             this.btbExtent.TabIndex = 4;
             this.btbExtent.Text = ":::";
-            this.btbExtent.UseVisualStyleBackColor = true;
+            this.btbExtent.UseVisualStyleBackColor = false;
+            this.btbExtent.Visible = false;
             // 
             // btnCategory
             // 
+            this.btnCategory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(179)))), ((int)(((byte)(179)))));
             this.btnCategory.Location = new System.Drawing.Point(1018, 349);
             this.btnCategory.Name = "btnCategory";
             this.btnCategory.Size = new System.Drawing.Size(57, 33);
             this.btnCategory.TabIndex = 4;
             this.btnCategory.Text = ":::";
-            this.btnCategory.UseVisualStyleBackColor = true;
+            this.btnCategory.UseVisualStyleBackColor = false;
+            this.btnCategory.Visible = false;
             // 
             // btnCancel
             // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCancel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnCancel.Location = new System.Drawing.Point(762, 409);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(96, 43);
             this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Visible = false;
             // 
             // btnSave
             // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnSave.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnSave.Location = new System.Drawing.Point(902, 409);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(96, 43);
             this.btnSave.TabIndex = 3;
             this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Visible = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "actualizar-datos.png");
+            this.imageList1.Images.SetKeyName(1, "agregar-archivo.png");
+            this.imageList1.Images.SetKeyName(2, "cerrar-sesion.png");
+            this.imageList1.Images.SetKeyName(3, "eliminar.png");
+            this.imageList1.Images.SetKeyName(4, "grafico.png");
+            this.imageList1.Images.SetKeyName(5, "informacion.png");
             // 
             // Articles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(1149, 668);
             this.Controls.Add(this.btnCategory);
             this.Controls.Add(this.btbExtent);
@@ -271,6 +328,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtv_List);
             this.Name = "Articles";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Articles";
             ((System.ComponentModel.ISupportInitialize)(this.dtv_List)).EndInit();
             this.ResumeLayout(false);
@@ -301,5 +359,6 @@
         private System.Windows.Forms.Button btnCategory;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
---
+++ CRUD_Sol/Articles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_Sol
+{
+    public partial class Articles : Form
+    {
+        public Articles()
+        {
+            InitializeComponent();
+        }
+
+        #region this Variables
+        int nCode_ar = 0;
+        int nCode_me = 0;
+        int nCode_ca = 0;
+        #endregion
+
+
+        #region this Methods
+        private void State_text(bool lState) 
+        {
+            txtArticles.ReadOnly= !lState;
+            txtBrand.ReadOnly= !lState;
+        }
+        #endregion
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            this.State_text(true);
+        }
+    }
+}
